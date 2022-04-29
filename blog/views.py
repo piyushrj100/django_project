ca350f010809f571f63a0961a3c405978eb7f54b
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-29 17:17:05 +0530

    added html template

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    #return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request) :
-    return HttpResponse('<h1>About Blog</h1>')
+    #return HttpResponse('<h1>About Blog</h1>')
+    return render(request,'blog/about.html')
 
 # Create your views here.
